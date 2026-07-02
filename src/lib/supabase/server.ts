0af9c1afee2e4bc4c9a200ecc5685180056c95d1
Furqan Ahmed <furqanahmedshaikh5@gmail.com>
2026-07-02 17:08:28 +0500

    Replace ANON_KEY with PUBLISHABLE_KEY in Supabase client

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -6,7 +6,7 @@ export const createSupabaseServerClient = () => {
   const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
         get(name: string) {
